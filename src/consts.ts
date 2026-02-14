b9a6e4fc185ce78cc260e0fc48c67029aff25490
Felipe Adeildo <contato@felipeadeildo.com>
2026-02-14 10:50:00 -0300

    chore: reset changes

--- src/consts.ts
+++
@@ -1,9 +0,0 @@
-export const SITE_TITLE = {
-  pt: 'LABORA - FGV EMAp',
-  en: 'LABORA - FGV EMAp',
-} as const
-
-export const SITE_DESCRIPTION = {
-  pt: 'Laboratório de Inteligência Artificial, Biomodelagem e Análise de Resposta a Surtos',
-  en: 'Laboratory of Artificial Intelligence, Biomodeling, and Outbreak Response Analysis',
-} as const
